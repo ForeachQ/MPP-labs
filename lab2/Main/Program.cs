1d1be11c82a818b56c21379caeff06770a166cdc
Ilya Golovin <foreachq@gmail.com>
2021-12-07 22:21:30 +0300

    Pushing lab2

---
+++ lab2/Main/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Faker.Faker f = new Faker.Faker();
+            User ff = f.Create<User>();
+            //ICollection<int> ff = f.Create<List<int>>();
+            Console.WriteLine(JsonConvert.SerializeObject(ff,Formatting.Indented));
+
+            //Console.WriteLine(ff.ToString());
+        }
+    }
+}
---
+++ lab2/TestProject1/CollectionTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Faker;
+
+namespace TestProject1
+{
+    class CollectionTest
+    {
+        [Test]
+        public void CollectionCreate()
+        {
+            Faker.Faker faker = new Faker.Faker();
+
+            List<List<char>> difficult = faker.Create<List<List<char>>>();
+            char b = difficult[0][0];
+            Assert.AreEqual(b == default(char), false, "Item in List of Lists hasn't been initialized.");
+        }
+    }
+}
