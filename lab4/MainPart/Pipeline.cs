6de22701b7bdc0ace209d93815f7decb7f41f379
Ilya Golovin <foreachq@gmail.com>
2021-12-15 02:02:39 +0300

    Add Pipeline and Main method

---
+++ lab4/MainPart/Pipeline.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using TestGeneratorLib;
+using System.Threading;
+
+namespace MainPart
+{
+    public class Pipeline
+    {
+        public Task Generate(IEnumerable<string> files, string pathToGenerated)
+        {
+            var execOptions = new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 2 };
+            var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+            var downloadStringBlock = new TransformBlock<string, string>
+            (
+                async path =>
+                {
+                    using (var reader = new StreamReader(path))
+                    {
+                        Console.WriteLine(Thread.CurrentThread.ManagedThreadId + "  reading " + path);
+                        return await reader.ReadToEndAsync();
+                       
+                    }
+                },
+                execOptions
+            );
+            var generateTestsBlock = new TransformManyBlock<string, KeyValuePair<string, string>>
+            (
+                async sourceCode =>
+                {
+                    var fileInfo = await Task.Run(() => CodeAnalyzer.GetFileInfo(sourceCode));
+                    Console.WriteLine("zzz");
+
+                    return await Task.Run(() => TestsGenerator.GenerateTests(fileInfo));
+                    
+                },
+                execOptions
+            );
+            var writeFileBlock = new ActionBlock<KeyValuePair<string, string>>
+            (
+                async fileNameCodePair =>
+                {
+                    Console.WriteLine("ohoho");
+                    using (var writer = new StreamWriter(pathToGenerated + '\\' + fileNameCodePair.Key + ".cs"))
+                    {
+                        Console.WriteLine("hhh"+fileNameCodePair.Key);
+                        await writer.WriteAsync(fileNameCodePair.Value);
+                    }
+                },
+                execOptions
+            );
+            downloadStringBlock.LinkTo(generateTestsBlock, linkOptions);
+            generateTestsBlock.LinkTo(writeFileBlock, linkOptions);
+
+            foreach (var file in files)
+            {
+                downloadStringBlock.Post(file);
+                Console.WriteLine(file);
+            }
+
+            downloadStringBlock.Complete();
+            return writeFileBlock.Completion;
+        }
+    }
+}
---
+++ lab4/MainPart/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MainPart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pathToFolder = "D:\\Projects\\mpp-labs\\lab4\\MainPart\\Files";
+            var pathToGenerated = @"D:\\Projects\\mpp-labs\\lab4\\GeneratedTests\\GeneratedFiles";
+            
+            if (!Directory.Exists(pathToFolder))
+            {
+                Console.WriteLine($"Couldn't find directory {pathToFolder}");
+                return;
+            }
+            if (!Directory.Exists(pathToGenerated))
+            {
+                Directory.CreateDirectory(pathToGenerated);
+            }
+
+            var allFiles = Directory.GetFiles(pathToFolder);
+
+            var files = from file in allFiles
+                    where file.Substring(file.Length - 3) == ".cs"
+                    select file;
+
+            Task task =  new Pipeline().Generate(files, pathToGenerated);
+            task.Wait();
+            //Thread.Sleep(2000);
+            Console.WriteLine("end.");
+        }
+    }
+}
